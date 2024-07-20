1357b7f329574f20860150074fb0a3791b9de83b
Tero Vuorela <tero.vuorela@iki.fi>
2024-07-20 10:09:09 +0300

    Add todos to main fn

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use crate::model::game::Game;
 
+mod io;
 pub mod model;
 
 fn main() {
-    let game: Option<Game> = None;
     let game = Game::new("game01", 32, 2);
 
-    println!("Hello, world!");
+    // TODO interpret cli command
+
+    // TODO 'show map and players'
+    // TODO 'generate report for single player'
+
+    // TODO 'run turn'
+    //  - read data
+    //  - perform players actions
+    //  - growing phase
+    //  - collect energies
+    //  - save data to new files
+
+    println!("DONE");
 }
