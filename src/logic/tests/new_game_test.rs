af9e1fa22f45bd0b45dd36ba990ce83b066fd034
Tero Vuorela <tero.vuorela@iki.fi>
2024-07-30 08:53:38 +0300

    Add tests with different map sizes

--- src/logic/tests/new_game_test.rs
+++ src/logic/tests/new_game_test.rs
@@ -7,8 +7,61 @@ mod text_io_tests {
     fn should_calculate_initial_positions() {}
 
     #[test]
+    fn should_create_game_with_1_player() {
+        // one player game is used just for testing some features
+        let game = create_new_game_with_initial_setup("game01", AmountOfPlayers::One);
+
+        let text_map = convert_to_text_map_3w(game);
+
+        // for debugging
+        // TODO option to turn off?
+        print!("{}", text_map);
+
+        // TODO find out players and verify initial formation
+    }
+
+    #[test]
     fn should_create_game_with_4_players() {
         let game = create_new_game_with_initial_setup("game01", AmountOfPlayers::Four);
+
+        let text_map = convert_to_text_map_3w(game);
+
+        // for debugging
+        // TODO option to turn off?
+        print!("{}", text_map);
+
+        // TODO find out players and verify initial formation
+    }
+
+    #[test]
+    fn should_create_game_with_9_players() {
+        let game = create_new_game_with_initial_setup("game01", AmountOfPlayers::Nine);
+
+        let text_map = convert_to_text_map_3w(game);
+
+        // for debugging
+        // TODO option to turn off?
+        print!("{}", text_map);
+
+        // TODO find out players and verify initial formation
+    }
+
+    #[test]
+    fn should_create_game_with_16_players() {
+        let game = create_new_game_with_initial_setup("game01", AmountOfPlayers::Sixteen);
+
+        let text_map = convert_to_text_map_3w(game);
+
+        // for debugging
+        // TODO option to turn off?
+        print!("{}", text_map);
+
+        // TODO find out players and verify initial formation
+    }
+
+    #[test]
+    fn should_create_game_with_25_players() {
+        let game = create_new_game_with_initial_setup("game01", AmountOfPlayers::TwentyFive);
 
         let text_map = convert_to_text_map_3w(game);
 
